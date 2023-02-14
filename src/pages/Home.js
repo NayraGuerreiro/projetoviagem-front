317cfebdc8718f139f36ea45dead186b274e31ab
Milene Fukuda <milene.fukuda@gmail.com>
2023-02-14 20:13:55 +0000

    update imgur

---
+++ src/pages/Home.js
@@ -0,0 +1,37 @@
+import api from "../utils/api";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+function Home() {
+  const [travels, setTravels] = useState([]);
+  console.log(travels);
+
+  useEffect(() => {
+    async function fetchTravels() {
+      try {
+        const response = await api.get("/travels");
+        setTravels(response.data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchTravels();
+  }, []);
+  return (
+    <>
+      <h1> Titulo</h1>
+
+      {travels.map((currentCountry) => {
+        return (
+          <>
+            <strong key={currentCountry.id}>
+              {currentCountry.attributes.country}
+            </strong>
+            {/* <img src={currentCountry.attributes.image} alt="foto da cidade" /> */}
+          </>
+        );
+      })}
+    </>
+  );
+}
+export default Home;
