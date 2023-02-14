72ea451220b31d6878bc9046b49cdd95772191b7
Milene Fukuda <milene.fukuda@gmail.com>
2023-02-14 21:50:09 +0000

    navbar e footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Form from "./pages/Form";
+import NavBar from "./pages/NavBar";
+import Footer from "./pages/Footer";
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/form" element={<Form />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
