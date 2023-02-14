9ca26922f21e4662b60065fbbe7e93a2326c5bf9
nayra <guerreironayra@gmail.com>
2023-02-14 16:23:34 -0300

    criação do formulario

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,10 +23,15 @@ function Home() {
 
       {travels.map((currentCountry) => {
         return (
-          <strong key={currentCountry.id}>
-            {currentCountry.attributes.country}
-          </strong>
-          //<img src={currentCountry.attributes.image} alt="foto da cidade" />
+          <div>
+            <strong key={currentCountry.id}>
+              {currentCountry.attributes.country}
+            </strong>
+            {/* //<img src={currentCountry.attributes.image} alt="foto da cidade" /> */}
+            <Link to="/form">
+              <button> Quero fazer essa viagem!</button>
+            </Link>
+          </div>
         );
       })}
     </>
